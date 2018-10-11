5d16328b611d2af159542029193a784319e1f855
Cobey A <cja1093@rit.edu>
2018-10-10 23:56:51 -0400

    A03 Deliverable

--- A03 - LERP/AppClass.cpp
+++ A03 - LERP/AppClass.cpp
@@ -2,7 +2,7 @@
 void Application::InitVariables(void)
 {
 	//Change this to your name and email
-	m_sProgrammer = "Alberto Bobadilla - labigm@rit.edu";
+	m_sProgrammer = "Cobey Adekanbi - cja1093@rit.edu";
 	
 	//Set the position and target of the camera
 	//(I'm at [0,0,10], looking at [0,0,0] and up is the positive Y axis)
@@ -18,6 +18,11 @@ void Application::InitVariables(void)
 		m_v4ClearColor = vector4(ZERO_V3, 1.0f);
 	}
 	
+	// Index vector for keeping track of each circles lerp targets
+	for (int i = 0; i < m_uOrbits; i++) {
+		m_vIndexes.push_back(0);
+	}
+
 	//if there are no segments create 7
 	if(m_uOrbits < 1)
 		m_uOrbits = 7;
@@ -28,14 +33,25 @@ void Application::InitVariables(void)
 	uint uColor = 650; //650 is Red
 	//prevent division by 0
 	float decrements = 250.0f / (m_uOrbits > 1? static_cast<float>(m_uOrbits - 1) : 1.0f); //decrement until you get to 400 (which is violet)
-	/*
-		This part will create the orbits, it start at 3 because that is the minimum subdivisions a torus can have
-	*/
+
+	// Create orbits
+	// -------------
 	uint uSides = 3; //start with the minimal 3 sides
 	for (uint i = uSides; i < m_uOrbits + uSides; i++)
 	{
 		vector3 v3Color = WaveLengthToRGB(uColor); //calculate color based on wavelength
 		m_shapeList.push_back(m_pMeshMngr->GenerateTorus(fSize, fSize - 0.1f, 3, i, v3Color)); //generate a custom torus and add it to the meshmanager
+		
+		std::vector<vector3> tempVec;
+		// Generate Locations for stop list
+		for (int j = 0; j < i; j++) {
+			 tempVec.push_back(vector3(cos((2 * PI / i) * j), sin((2 * PI / i) * j), 0.0) * ((i + 2) / uSides));
+		}
+		// Set give list to list of stoplists
+		m_stopsList.push_back(tempVec);
+		std::cout << tempVec[0].x << std::endl;
+		m_currentPositions.push_back(m_stopsList[i - uSides][0]);
+		
 		fSize += 0.5f; //increment the size for the next orbit
 		uColor -= static_cast<uint>(decrements); //decrease the wavelength
 	}
@@ -55,22 +71,51 @@ void Application::Display(void)
 {
 	// Clear the screen
 	ClearScreen();
+	
+	// Timer magic
+	static float fTimer = 0;
+	static uint uClock = m_pSystem->GenClock();
+	fTimer += m_pSystem->GetDeltaTime(uClock);
+	static float fPercentage = MapValue(fTimer, 0.0f, 2.0f, 0.0f, 1.0f);
+
+	// Start & End Positions
+	vector3 start;
+	vector3 end;
 
 	matrix4 m4View = m_pCameraMngr->GetViewMatrix(); //view Matrix
 	matrix4 m4Projection = m_pCameraMngr->GetProjectionMatrix(); //Projection Matrix
 	matrix4 m4Offset = IDENTITY_M4; //offset of the orbits, starts as the global coordinate system
-	/*
-		The following offset will orient the orbits as in the demo, start without it to make your life easier.
-	*/
+
 	//m4Offset = glm::rotate(IDENTITY_M4, 1.5708f, AXIS_Z);
 
 	// draw a shapes
 	for (uint i = 0; i < m_uOrbits; ++i)
 	{
+		// Reset when percentage is >= 1.0f
+		if (fPercentage >= 1.0f) {
+			m_vIndexes[i] += 1;
+			fTimer = m_pSystem->GetDeltaTime(uClock);
+			m_vIndexes[i] %= m_stopsList[0].size();
+		}
+
+		// Set start end values for this current iteration
+		start = m_stopsList[i].at(m_vIndexes[i]);
+		if (m_vIndexes[i] >= m_stopsList[i].size() - 1) {
+			m_vIndexes[i] = 0;
+		}
+		else {
+			m_vIndexes[i]++;
+		}
+		end = m_stopsList[i].at(m_vIndexes[i]);
+		
+		// Lerp (Still messin up)
+		m_currentPositions[i] = glm::lerp(start, end, fPercentage);
+
+		// Add mesh to render list
 		m_pMeshMngr->AddMeshToRenderList(m_shapeList[i], glm::rotate(m4Offset, 1.5708f, AXIS_X));
 
 		//calculate the current position
-		vector3 v3CurrentPos = ZERO_V3;
+		vector3 v3CurrentPos = m_currentPositions[i];
 		matrix4 m4Model = glm::translate(m4Offset, v3CurrentPos);
 
 		//draw spheres
--- A03 - LERP/AppClass.h
+++ A03 - LERP/AppClass.h
@@ -20,7 +20,10 @@ class Application
 {
 	uint m_uOrbits = 0; //number of shapes starting at 3 and increasing in sides
 	std::vector<uint> m_shapeList; //shape index for circles
-	String m_sProgrammer = "Alberto Bobadilla - labigm@rit.edu";
+	std::vector<vector3> m_currentPositions;
+	std::vector<int> m_vIndexes;
+	std::vector<std::vector<vector3>> m_stopsList;
+	String m_sProgrammer = "Cobey Adekanbi - cja1093@rit.edu";
 	
 private:
 	static ImGuiObject gui; //GUI object
